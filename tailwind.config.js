f05de86af4a43699c41c58fb2b8577442e3fb4c2
hh65 <hh65@sussex.ac.uk>
2024-10-27 12:36:22 +0000

    tailwind config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 // tailwind.config.js
 module.exports = {
-  content: ["./src/**/*.{html,css}"], // Adjust paths as needed
+  content: [
+    "./index.html",           // Include index.html in the root
+    "./src/**/*.{html,css}"    // Include all HTML and CSS files in the src directory
+  ],
   theme: {
     extend: {
       backgroundImage: {
         'full-background': "url('../assets/images/backgrounds/childhood_fear.jpg')",
-        // Add more custom backgrounds here if needed
       },
       backgroundSize: {
         'cover': 'cover',
@@ -15,13 +17,12 @@ module.exports = {
         'center': 'center',
         'top': 'top',
         'bottom': 'bottom',
-        // Add more positions if needed
       },
       fontFamily: {
-        heading: ['Montserrat', 'sans-serif'], // Custom font family for headings
+        heading: ['Montserrat', 'sans-serif'],
       },
       colors: {
-        'dark-green': '#006400', // Custom dark green color
+        'dark-green': '#006400',
       },
     },
   },
